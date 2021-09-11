0aab4136dc4b3febb133cceab5e2ff90c9e78a4f
everett-tw <63500700+everett-tw@users.noreply.github.com>
2021-09-11 13:41:33 -0700

    Delete key_finder.py

--- key_finder.py
+++
@@ -1,9 +0,0 @@
-#spotify keys
-client_secret = 'cbd19695cc80453abf7bee9ebf30e9a7'
-client_id = '7639061aab8e4afa9b23b9f98a8bd2b3'
-
-#last.fm keys
-last_fm_api_key = '31fd73eff8aa83d7cecf3ff621bf8058'
-shared_secret_lastfm = '1ebb6ae8998c72640c784768f6a2e1c4'
-
-audio_db_api_key = '523532'
