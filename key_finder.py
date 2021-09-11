87256b0f95aca5e1169925946ed35a8cff8cb927
everett-tw <everett.waterman@gmail.com>
2021-09-11 12:24:22 -0700

    working in flask

--- key_finder.py
+++ key_finder.py
@@ -5,3 +5,5 @@ client_id = '7639061aab8e4afa9b23b9f98a8bd2b3'
 #last.fm keys
 last_fm_api_key = '31fd73eff8aa83d7cecf3ff621bf8058'
 shared_secret_lastfm = '1ebb6ae8998c72640c784768f6a2e1c4'
+
+audio_db_api_key = '523532'
