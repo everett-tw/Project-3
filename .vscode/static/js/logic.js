a2f61d4ae9a88afabf30dea9236024dc6c016a7d
Luceroap <57282933+Luceroap@users.noreply.github.com>
2021-09-11 21:48:59 -0700

    web page

---
+++ .vscode/static/js/logic.js
@@ -0,0 +1,12 @@
+var myMap = L.map("map", {
+    center: [37.0409472, -122.028032],
+    zoom: 5
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  // Use this link to get the GeoJSON data.
+ 
